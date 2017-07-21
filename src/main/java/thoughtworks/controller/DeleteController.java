9a5da8c93630aebd489ff7d679c09340aa333541
Naveen Srivastava <naveen.srivastava@thetrainline.com>
2017-07-21 15:27:15 +0530

    Removed Unused imports

--- src/main/java/thoughtworks/controller/DeleteController.java
+++ src/main/java/thoughtworks/controller/DeleteController.java
@@ -1,8 +1,7 @@
 package thoughtworks.controller;
 
-import com.sun.deploy.net.HttpResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
