ca5c54e646e1e8bd3b210ec69bd9ff28ff28d293
Dhanush <dhanushd@thoughtworks.com>
2017-07-21 11:25:40 +0530

    Adds project skeleton

---
+++ src/main/java/thoughtworks/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package thoughtworks.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return name;
+    }
+}
