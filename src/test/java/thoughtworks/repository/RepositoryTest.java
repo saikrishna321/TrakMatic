ad29320cb3365862f561980721bdb847320584a2
saikrishna321 <saikrishna321@yahoo.com>
2017-07-22 23:17:59 +0530

    added Assertion

--- src/test/java/thoughtworks/repository/RepositoryTest.java
+++ src/test/java/thoughtworks/repository/RepositoryTest.java
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.JSONException;
 import org.junit.Test;
 import thoughtworks.respository.Repository;
-
 import java.util.HashMap;
 
-/**
- * Created by bsneha on 21/07/17.
- */
+import static org.junit.Assert.assertTrue;
+
+
 public class RepositoryTest {
+
     @Test
     public void shouldStoreStringJsonToRepository() throws JSONException, JsonProcessingException {
         Repository repository = new Repository();
         repository.store("{\"name\":\"sherlock\",\"foo\":\"bar\"}");
         repository.store("{\"foo\":\"bar\",\"js\":\"react\"}");
         HashMap<String, String> parameters = new HashMap<>();
-        parameters.put("foo","bar");
-        repository.readData(parameters);
+        parameters.put("foo", "bar");
+        assertTrue(repository.readData(parameters).size() == 2);
     }
 }
